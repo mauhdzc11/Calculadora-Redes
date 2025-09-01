57f2c0d576e219676f8d9ebecffe8b22d22cc1a0
mauhdzc11 <maurixddd11@gmail.com>
2025-09-01 15:42:48 -0600

    Create Calculadora.c

---
+++ Calculadora.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+float division(int x,int y);
+int suma(int x, int y);
+int resta(int x, int y);
+int multiplicacion(int x,int y);
+int modulo(int x,int y);
+
+float division(int x, int y)
+{
+
+}
+
+int suma(int x, int y)
+{
+
+}
+
+int resta(int x, int y)
+{
+
+}
+
+int multiplicacion(int x, int y)
+{
+
+}
+
+int modulo(int x, int y)
+{
+  while(x>=y)
+  {
+    x-=y;
+  }
+  return x;
+}
+
+
+int main()
+{
+  int x, y, opc;
+  }
+}
